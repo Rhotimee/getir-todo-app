e40eb84295ab2fed201d723ccb337a3729af5af1
Yemitan Isaiah Olurotimi <yemitanisaiah@gmail.com>
2021-01-14 03:15:18 +0300

    chore: remove commented code

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// baseURL: 'https://simple-todo-nest.herokuapp.com/todos',
 const Axios = axios.create({
   baseURL: 'https://simple-todo-nest.herokuapp.com/todos',
 });
